43f01ba28d8756adac78c8bd821a3bcca7f1fc6a
dpolve <dawde.din@gmail.com>
2020-05-06 09:03:03 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { RegioniComponent } from './regioni/regioni.component';
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, RegioniComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/regioni/regioni.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regioni',
+  templateUrl: './regioni.component.html',
+  styleUrls: ['./regioni.component.css']
+})
+export class RegioniComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
