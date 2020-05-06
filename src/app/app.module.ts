ab510067fb50d5bf029a754de2d21127bced927b
dpolve <dawde.din@gmail.com>
2020-05-06 09:07:47 +0200

    1 file deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
@@ -14,7 +14,7 @@ import { ProvinceComponent } from './province/province.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, RegioniComponent, ProvinceComponent ],
+  declarations: [ AppComponent, , RegioniComponent, ProvinceComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
